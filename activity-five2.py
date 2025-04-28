aa8a9329a037a968ff8a69d3576fa5f75ed99f07
acarter-2025 <a.carter@snhu.edu>
2025-04-27 22:39:18 -0500

    Update activity-five2.py

--- activity-five2.py
+++ activity-five2.py
@@ -1,33 +1,33 @@
-# For Module 5
+from threading import Semaphore, Thread
+import time
 
-from threading import *         
-import time         
-  
-# creating thread instance where count = 3 
-obj = Semaphore(3)         
-  
-# creating instance 
-def display(name):     
-    
-    # calling acquire method 
-    obj.acquire()                 
-    for i in range(5): 
-        time.sleep(3) 
-        print(name) 
-          
-        # calling release method 
-        obj.release()     
-          
-# creating multiple thread  
-t1 = Thread(target = display , args = ('Thread-1',)) 
-t2 = Thread(target = display , args = ('Thread-2',)) 
-t3 = Thread(target = display , args = ('Thread-3',)) 
-t4 = Thread(target = display , args = ('Thread-4',)) 
-t5 = Thread(target = display , args = ('Thread-5',)) 
-  
-# calling the threads  
-t1.start() 
-t2.start() 
-t3.start() 
-t4.start() 
-t5.start()
+# Initialize a semaphore with a maximum count of 3
+# This limits the maximum number of threads that can run concurrently
+semaphore = Semaphore(3)
+
+# Function to display thread names with structured output
+def display(name):
+    # Acquire the semaphore before proceeding
+    semaphore.acquire()
+    try:
+        for i in range(5):
+            # Simulate work with a delay of 3 seconds
+            time.sleep(3)
+            # Print the thread name along with a timestamp and iteration count
+            print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {name}: Iteration {i+1}")
+    finally:
+        # Release the semaphore to allow other threads to proceed
+        semaphore.release()
+
+# Create multiple threads with unique names
+threads = [
+    Thread(target=display, args=(f"Thread-{i+1}",)) for i in range(5)
+]
+
+# Start all threads
+for thread in threads:
+    thread.start()
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
